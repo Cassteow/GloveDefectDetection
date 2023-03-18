847df7b1a2bdcb41df620cc17c4a9aa0dd434ffb
Cassandrea Teow <cassteowthg@gmail.com>
2023-03-18 22:42:05 +0800

    RUBBER GLOVE TEAR (EDIT SAMPLE PICS)

--- main/cloth_glove_defect_detection.m
+++ main/cloth_glove_defect_detection.m
@@ -167,6 +167,10 @@ function ExitButton_Callback(hObject, eventdata, handles)
 % hObject    handle to ExitButton (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+set(handles.openingAns,'String',"");
+set(handles.threadAns,'String',"");
+set(handles.stainAns,'String',"");
+
 close(cloth_glove_defect_detection);
 
 
